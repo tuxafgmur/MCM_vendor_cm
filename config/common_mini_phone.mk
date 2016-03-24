7d90097c2d8948f9f7c7d2fdb284b883e8f72932
tuxafgmur <tuxafgmur@users.noreply.github.com>
2016-03-24 03:31:42 +0100

    Removes bootanimation

--- config/common_mini_phone.mk
+++ config/common_mini_phone.mk
@@ -8,9 +8,4 @@ $(call inherit-product, vendor/cm/config/sound_config.mk)
 PRODUCT_PACKAGES += \
     LatinIME
 
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/320.zip:system/media/bootanimation.zip
-endif
-
 $(call inherit-product, vendor/cm/config/telephony.mk)
--- config/common_mini_tablet_wifionly.mk
+++ config/common_mini_tablet_wifionly.mk
@@ -7,8 +7,3 @@ $(call inherit-product, vendor/cm/config/sound_config.mk)
 # Required CM packages
 PRODUCT_PACKAGES += \
     LatinIME
-
-ifeq ($(TARGET_SCREEN_WIDTH) $(TARGET_SCREEN_HEIGHT),$(space))
-    PRODUCT_COPY_FILES += \
-        vendor/cm/prebuilt/common/bootanimation/800.zip:system/media/bootanimation.zip
-endif
