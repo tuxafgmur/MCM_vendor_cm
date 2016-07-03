bbdae79455ac2d8515cd13f03afe156580cd6eb0
tuxafgmur <tuxafgmur@users.noreply.github.com>
2016-07-04 00:47:50 +0200

    Builds 'su' in all variants

--- config/common.mk
+++ config/common.mk
@@ -174,9 +174,12 @@ PRODUCT_PROPERTY_OVERRIDES += \
 ifneq ($(TARGET_BUILD_VARIANT),user)
 PRODUCT_PACKAGES += \
     procmem \
-    procrank \
-    su
+    procrank
 endif
+
+# These package are not excluded from user builds
+PRODUCT_PACKAGES += \
+    su
 
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.sys.root_access=0
